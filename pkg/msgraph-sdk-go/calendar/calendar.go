7320ffe29d132a31f1e52f60117095ab7825c5eb
Emily Zhou <emzho@microsoft.com>
2018-10-31 13:15:53 -0700

    Adding methods to ICalendar interface

--- pkg/msgraph-sdk-go/calendar/calendar.go
+++ pkg/msgraph-sdk-go/calendar/calendar.go
@@ -1,5 +1,7 @@
 package calendar
 
+import "time"
+
 type Calendar struct {
 	CanEdit bool `json:"canEdit"`
 	CanShare bool `json:"canShare"`
@@ -17,4 +19,21 @@ type Calendar struct {
 	Events interface{} `json:"events",omitempty` // TODO: Events EventCollection
 	MultiValueLegacyExtendedProperties interface{} `json:"multiValueLegacyExtendedProperties",omitempty` // TODO: MultiValueLegacyExtendedPropertiesCollection
 	SingleValueLegacyExtendedProperties interface {} `json:"singleValueLegacyExtendedProperties",omitempty` // TODO: SingleValueLegacyExtendedPropertiesCollection
+}
+
+type ICalendar interface {
+	ListCalendar() interface{} // TODO: CalendarCollection struct
+	CreateCalendar(calendar Calendar) Calendar
+	GetCalendar() Calendar
+	Update(color string, isDefaultCalendar bool, name string) Calendar
+	Delete()
+	ListCalendarView(startTime time.Time, endTime time.Time) interface{} // TODO: EventCollection struct
+	ListEvents()
+	CreateEvent() interface{} // TODO: Event struct
+	FindMeetingTimes() interface{} // TODO: MeetingTimeSuggestionsResult struct
+	GetSchedule() interface{} // TODO: ScheduleInformationCollection struct and ScheduleInformation struct
+	CreateSingleValueExtendedProperty() Calendar
+	GetCalendarWithSingleValueExtendedProperty() Calendar
+	CreateMultiValueExtendedProperty() Calendar
+	GetCalendarWithMultiValueExtendedProperty() Calendar
 }
